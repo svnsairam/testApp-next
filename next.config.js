d090f2f73ce447aeec85b022c4c5bc9ae4b70e6d
Sairam Sakhamuri <svnsairam@google.com>
2023-10-13 09:21:53 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,76 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+  } = require('next/constants')
+  
+  module.exports = (phase) => {
+  
+    // npm run dev or next dev
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  
+    // npm run build or next build
+    const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
+  
+    // npm run build or next build
+    const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1';
+  
+    
+  
+    const env = {
+        TITLE: (() => {
+            if(isDev) return 'Title Dev'
+            if(isProd) return 'Title Prod'
+            if(isStaging) return 'Title Stg'
+        })()
+    }
+  
+  
+    const rewrites = () => {
+        return [
+          {
+              source: '/configJsonRewrite',
+              destination: '/about'
+          },
+          {
+              source: '/confictRW',
+              destination: '/'
+          }
+        ]
+    }
+  
+    
+  
+    const headers = () => {
+        return [
+          {
+              source: '/about',
+              headers: [
+                  {
+                      key: "x-custom-header-1",
+                      value: "my custom header 1"
+                  },
+                  {
+                      key: "x-next-conf-key",
+                      value: "x-next-coonf-value-header"
+                  }
+              ]
+          },
+          {
+              source: '/my-custom-page',
+              headers: [
+                  {
+                      key: "x-custom-page-header-1111",
+                      value: "my custom-page header 11001"
+                  }
+              ]
+          }
+        ]
+    }
+  
+  
+    return {
+        env,
+        rewrites,
+        headers
+    }
+  }
